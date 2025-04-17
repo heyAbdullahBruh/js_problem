71e1ae8a8c6365dac17718b964e8debf304196d6
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-04-17 10:48:18 +0600

    funcComp.js

---
+++ funcComp.js
@@ -0,0 +1,15 @@
+/*Function Composition*/
+
+var compose = function (functions) {
+  return function (x) {
+    return functions.reduceRight((acc, fn) => fn(acc), x);
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+const fn = compose([x => x + 1, x => 2 * x])
+console.log(fn(4) );// 9
